62a0016fedf7729b553905cce79d9425ee8d04aa
YamkelaQhogwana <133680348+YamkelaQhogwana@users.noreply.github.com>
2023-06-07 16:49:25 +0200

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=flowervisa';
-$user_name = 'root';
-$user_password = 'Sibu1978!';
+$db_name = 'mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_8bc3cf847a4842e';
+$user_name = 'bdafc5385767a6';
+$user_password = '987cd7d4';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
